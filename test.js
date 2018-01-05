900dc7e1737b994cc9073cfb2d87e480a4983fb9
chenyi <2091320109@qq.com>
2018-01-05 22:21:48 +0800

    Initial commit

---
+++ test.js
@@ -0,0 +1,9 @@
+function test1() {
+    // var colors = ["red", "green", "blue"]
+    // alert(colors)
+    // colors.shift()
+    // alert(colors)
+
+    var values = [0, 1, 2, 3, 5, 4]
+    alert(new Date().toString())
+}
